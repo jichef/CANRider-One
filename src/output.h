49683436be5adaaa360f2bdcadd25204a9ba533e
jichef <55363964+jichef@users.noreply.github.com>
2025-04-15 01:35:05 +0200

    Update output.h
    Se elimina la referencia a la TFT

--- src/output.h
+++ src/output.h
@@ -3,14 +3,15 @@
 
 #include <Arduino.h>
 #include <BluetoothSerial.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7735.h>
+
 
 // Prototipos de funciones
-void initializeOutput(BluetoothSerial &btSerial, Adafruit_ST7735 &tftDisplay);
+void initializeOutput(BluetoothSerial &btSerial);
 void logToOutput(const String &message);
 void logToOutput_P(const char *message);
 void logToOutputln(const String &message);
-void displayBatteryState();
+
+
+
 
 #endif
