8956e4bcf40632cf9acae8fa36d5e8dc41260d20
jichef <55363964+jichef@users.noreply.github.com>
2025-04-15 01:37:08 +0200

    Update sms.h

--- src/sms.h
+++ src/sms.h
@@ -7,10 +7,8 @@
 // Prototipos de funciones para manejar SMS
 void sendSMS(TinyGsm &modem, const String &phoneNumber, const String &message);
 void checkForSMS(TinyGsm &modem);  // Modificado para aceptar 'modem' como argumento
-void readSMS(TinyGsm &modem);      // Modificado para aceptar 'modem' como argumento
-void deleteAllSMS(TinyGsm &modem); // Modificado para aceptar 'modem' como argumento
+void processSMS(TinyGsm &modem, const String &response);
 float getLastLat();  // Declarar la función para obtener la última latitud
 float getLastLon();  // Declarar la función para obtener la última longitud
-
 
 #endif
